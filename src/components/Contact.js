1c93e7003254365ff5c154bfc11b224d7727b482
John Sun <48033307+7even-7@users.noreply.github.com>
2023-09-17 23:49:40 -0700

    added email functionality with emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,43 @@
-import React from 'react'
+import React,{ useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope, faUser, faMessage, faPaperPlane} from '@fortawesome/free-solid-svg-icons'
+import emailjs, { send } from '@emailjs/browser';
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_55fcsgp', 'template_0651idt', form.current, 'd1NBbPf72gUIZaAax')
+      .then((result) => {
+          window.alert("Message sent.")
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <>
       <div className='contact'>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <div className="input-group">
-            <input type="text" id="name" required/>
+            <input type="text" id="name" name="user_name" required/>
             <label for="name"><FontAwesomeIcon icon={faUser} style={{color: "#772828",}}/> Your Name</label>
           </div>
           <div className="input-group">
-            <input type="text" id="number" required/>
+            <input type="text" id="number" name="user_phone" required/>
             <label for="number"><FontAwesomeIcon icon={faPhone} style={{color: "#772828",}}/> Phone #</label>
           </div>
           <div className="input-group">
-            <input type="email" id="email" required/>
+            <input type="email" id="email" name = "user_email" required/>
             <label for="email"><FontAwesomeIcon icon={faEnvelope} style={{color: "#772828",}}/> Email</label>
           </div>
           <div className="input-group">
-            <textarea id="message"  rows="7"/>
+            <textarea id="message"  name="message" rows="7"/>
             <label for="message"><FontAwesomeIcon icon={faMessage} style={{color: "#772828",}}/> Your Message</label>
           </div>
-          <button type='submit'><FontAwesomeIcon icon={faPaperPlane} style={{color: "#772828",}}/> Submit</button>
+          <button type='submit' value="Send"><FontAwesomeIcon icon={faPaperPlane} style={{color: "#772828",}}/> Submit</button>
         </form>
 
       </div>
